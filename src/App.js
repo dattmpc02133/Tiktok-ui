7807e7fa44fab3331d8ac5458b49d60d8641ef9a
dattmpc02133 <datgoodboy2002@gmail.com>
2022-09-25 16:58:57 +0700

    Cài đặt và cấu hình Prettier

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import { Routes, Route, Link } from "react-router-dom";
-import Homepage from "~/pages/Home";
-import ContactPage from "~/pages/Contact";
-import NewPage from "~/pages/New";
+import { Routes, Route, Link } from 'react-router-dom';
+import Homepage from '~/pages/Home';
+import ContactPage from '~/pages/Contact';
+import NewPage from '~/pages/New';
 
 function App() {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/contact">contact</Link>
-          </li>
-          <li>
-            <Link to="/new">new</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/new" element={<NewPage />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/new">new</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/new" element={<NewPage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
